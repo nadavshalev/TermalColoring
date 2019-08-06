80202b93463bb6acd656ddc2ce9a1364c55294aa
nadav shalev <nadav.shalev@campus.technion.ac.il>
2019-08-06 12:55:05 +0000

    init

---
+++ save_func.py
@@ -0,0 +1,14 @@
+import pickle
+
+
+def save_v(file_folder, file_object):
+    file_obj = open(file_folder, 'w+')
+    pickle.dump(file_object, file_obj)
+
+
+def save_v(file_folder):
+    file_obj = open(file_folder, 'r')
+    return pickle.load(file_obj)
+
+
+save_v('123', [1,2,3])
