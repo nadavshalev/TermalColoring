7408dda90cc1aecba158c1224edcc9f4bab32455
nadav.shalev <nadav.shalev@sipl-gpu3-u>
2019-08-27 09:56:53 +0300

    finish register func

--- matlab/thRegister.m
+++ matlab/thRegister.m
@@ -1,4 +1,8 @@
-function [RIG,tform, values] = thRegister(IT,IG)
+function [RIG,tform, value, values, methond] = thRegister(IT,IG, toPlot)
+if nargin < 3
+    toPlot = false;
+end
+methond = ["translation" "rigid" "similarity" "affine"];
 [DT, ~] = imgradient(IT,'prewitt');
 [DG, ~] = imgradient(IG,'prewitt');
 baseVal = norm(DG.*DT);
@@ -9,15 +13,11 @@ optimizer.MaximumIterations = 300;
 [RDG, ~] = imgradient(RIG,'prewitt');
 value = norm(RDG.*DT);
 values = value;
-
-[tmpIm,tmpForm] = imregister2(IG,IT,'rigid',optimizer,metric);
-[tmpDm, ~] = imgradient(tmpIm,'prewitt');
-tmpVal = norm(tmpDm.*DT);
-values = [values tmpVal];
-if tmpVal > value
-    value = tmpVal;
-    RIG = tmpIm;
-    tform = tmpForm;
+if toPlot
+    figure;
+    subplot(1,2,1); imshowpair(RDG,DT.^0.6);  title('translation')
+    subplot(1,2,2); imshowpair(RIG,IT); 
+    drawnow;
 end
 
 [tmpIm,tmpForm] = imregister2(IG,IT,'rigid',optimizer,metric);
@@ -29,8 +29,14 @@ if tmpVal > value
     RIG = tmpIm;
     tform = tmpForm;
 end
+if toPlot
+    figure;
+    subplot(1,2,1); imshowpair(tmpDm,DT.^0.6);  title('rigid')
+    subplot(1,2,2); imshowpair(tmpIm,IT); 
+    drawnow;
+end
 
-[tmpIm,tmpForm] = imregister2(IG,IT,'rigid',optimizer,metric);
+[tmpIm,tmpForm] = imregister2(IG,IT,'similarity',optimizer,metric);
 [tmpDm, ~] = imgradient(tmpIm,'prewitt');
 tmpVal = norm(tmpDm.*DT);
 values = [values tmpVal];
@@ -38,6 +44,29 @@ if tmpVal > value
     value = tmpVal;
     RIG = tmpIm;
     tform = tmpForm;
+end
+if toPlot
+    figure;
+    subplot(1,2,1); imshowpair(tmpDm,DT.^0.6);  title('similarity')
+    subplot(1,2,2); imshowpair(tmpIm,IT); 
+    drawnow;
+end
+
+
+[tmpIm,tmpForm] = imregister2(IG,IT,'affine',optimizer,metric);
+[tmpDm, ~] = imgradient(tmpIm,'prewitt');
+tmpVal = norm(tmpDm.*DT);
+values = [values tmpVal];
+if tmpVal > value
+    value = tmpVal;
+    RIG = tmpIm;
+    tform = tmpForm;
+end
+if toPlot
+    figure;
+    subplot(1,2,1); imshowpair(tmpDm,DT.^0.6);  title('affine')
+    subplot(1,2,2); imshowpair(tmpIm,IT); 
+    drawnow;
 end
 
 if baseVal > value
